e9516f504661ef3e929e1371c99008fbd6cc7311
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-21 17:48:06 +0600

    JavaScript Scope and Lexical Environment.
    Here, Practice on global scope, function scope, Block scope & Lexical Environmen.

--- index.js
+++ index.js
@@ -3,10 +3,41 @@
 // Block scope 
 
 // global scope
-let name = "Alex";
+let name = "Alex";//its a global variable;
 
 function sayHello(){
     console.log("Hello "+ name);
 }
-
 sayHello();
+
+// function scope 
+function greet() {
+    let message = "Good Morning!";
+    console.log(message);
+}
+greet();
+
+// Block scope 
+
+if(true){
+    let age = 25;
+    console.log(age);
+}
+// console.log(age);
+
+if(true){
+    var age1 = 25;
+    console.log(age1);
+}
+console.log(age1);
+
+//Lexical Environment
+function outer(){
+    let name = "BD";
+
+    function inner(){
+        console.log(name);
+    }
+    inner();
+}
+outer();
