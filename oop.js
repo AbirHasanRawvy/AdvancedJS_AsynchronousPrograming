68059e1727f44685b5ada19ca353ee427a0efcec
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-22 19:35:54 +0600

    OOP.

---
+++ oop.js
@@ -0,0 +1,17 @@
+//Encapculation: pack the data and method
+//Abstraction: hide unnessary data 
+//Inheritance: relation between one to another class
+//Polymorphism: overriding & overloading
+
+
+//Encapculation: pack the data and method
+class MyClass{
+    x=50;
+    myFun(){
+        console.log("Hello JS OOP");
+    }
+}
+
+let myObj = new MyClass()
+
+myObj.myFun()
