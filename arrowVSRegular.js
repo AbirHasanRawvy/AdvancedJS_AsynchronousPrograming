c2b05419ca431e6241c72dd860ac908e71d67136
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-23 11:37:18 +0600

    Arrow function VS Regular function.

--- arrowVSRegular.js
+++ arrowVSRegular.js
@@ -1,1 +1,13 @@
 // Arrow function vs Regular function;
+//Regular function;
+function add(a, b){
+    return a + b;
+}
+console.log(add(10,20));
+
+//Arraow function: Come into ES6(eagma script).
+let add1 = (c,d)=>{
+
+    return c + d;
+}
+console.log(add1(20,30));
