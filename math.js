1e0b4e8e14ee477fdfe5b3da18ae6599db20072c
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-23 19:41:47 +0600

    Import-Export
    Here, write down on single and default export.

---
+++ math.js
@@ -0,0 +1,15 @@
+//connected with modules;
+//here all are singlle export;
+export function add(a,b){
+    return a+b;
+}
+
+export function sub(a,b){
+
+    return a-b;
+}
+
+//default export;
+export default function mul(c,d){
+    return c*d;
+}
---
+++ modules.js
@@ -0,0 +1,7 @@
+//JavaScript Modules (ES6 import);
+import{add, sub} from "./math.js"
+import mul from "./math.js"
+
+console.log(add(10, 20));
+console.log(sub(50, 20));
+console.log(mul(50, 20));
