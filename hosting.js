763422c5a28418965bf813c8d7fe9c360bcce7b6
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-23 12:02:05 +0600

    Hosting.

---
+++ hosting.js
@@ -0,0 +1,14 @@
+//for function;
+hello();
+
+function hello() {
+    console.log("Hello from hosting...");
+}
+
+//for var come but with undefiend;
+console.log(age);
+var age = 25;
+
+//let and const is work like reffarence.const and let only work for TDZ: desd zone;
+console.log(age1);
+let age1 = 25;
