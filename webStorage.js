efe844ee9c7cbc6a02771ed662480dc384f804da
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-25 01:40:04 +0600

    Web Storage.
    Here is local storage and session storage.

---
+++ webStorage.js
@@ -0,0 +1,18 @@
+// //local storage and session storage;
+
+// localStorage.setItem("userName", "Alex");
+
+// //data received;
+// let name = localStorage.getItem("userName");
+
+// console.log(name);
+
+// //remove data;
+// localStorage.removeItem("userName");
+
+// sessionStorage.setItem("userName", "Alex");
+
+// sessionStorage.removeItem("userName");
+
+// localStorage.removeItem("clear");
+
