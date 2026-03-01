e8ca9ca32167014dcfce5c157b198a243f8c998b
Abir Hasan <abirhasanrawvy@gmail.com>
2026-03-01 19:48:51 +0600

    Class1.

---
+++ class1.js
@@ -0,0 +1,39 @@
+//Destructuring;
+
+//Array Destructuring;
+const colors = ["red", "green", "blue"];
+
+const [red, green, blue] = colors;
+
+console.log(red);
+console.log(green);
+console.log(blue);
+
+//only find out thir number;
+const colors1 = ["red", "green", "blue"];
+
+const [, , thirdColor] = colors1;
+console.log(thirdColor);
+
+//swap;
+let a = 4;
+let b = 7;
+
+[a, b]=[a, b];
+console.log(a);
+console.log(b);
+
+
+//Object Destructuring;
+const user = {
+    name: "Abir",
+    age: 15,
+};
+
+console.log(user.name);//dot notation;
+console.log(user["age"]);//bracket notation;
+
+const{name: myName, age} = user;
+
+console.log(myName);
+console.log(age);
